7051b8e2bc4cc6fd4437da66c1c6b9edd4794e5c
Joy Ajiboye <joyajiboye2016@gmail.com>
2022-06-08 17:22:51 +0100

    add font  and some sample styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Home from './pages/home';
 
 function App() {
-  return <div className='font-bold text-blue-900'>Loading Connect</div>;
+  return (
+    <div className='font-bold text-blue-900'>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/header';
+import './index.scss';
+//landing page
+const Home = () => {
+  return (
+    <div className='home'>
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
