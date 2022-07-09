d1f1f1646694b48d736ec34370077085c57fd154
sirsuccess <kanuamani@gmaail.com>
2022-07-09 22:46:25 +0100

    implement map

--- src/pages/app/Dashboard/index.jsx
+++ src/pages/app/Dashboard/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import DashboardLayout from "../../../components/DashboardLayout";
+import Map from "../../../components/map";
 import "./styles.scss";
 
 const Dashboard = () => {
@@ -9,7 +10,9 @@ const Dashboard = () => {
       <div className="dashboard">
         <div className="dashboard-left">left</div>
         <div className="dashboard-middle">midle</div>
-        <div className="dashboard-right">right</div>
+        <div className="dashboard-right">
+          <Map />
+        </div>
       </div>
     </DashboardLayout>
   );
