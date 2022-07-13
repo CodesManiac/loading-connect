25d23890bf95a1b8d05d29c9881a86264f3ebc58
Ruby <graceffiong@gmail.com>
2022-07-13 03:41:27 +0100

    Settings

--- src/pages/app/Settings/index.jsx
+++ src/pages/app/Settings/index.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import DashboardLayout from "../../../components/DashboardLayout";
+import Profile from "../../../components/settings/Profile";
+import Sidebar from "../../../components/settings/Sidebar";
 import "./styles.scss";
 
 const Dashboard = () => {
   return (
     <DashboardLayout>
-     settings
+     <div className="dashboard">
+        <div className="dashboard-left">
+          <Sidebar />
+        </div>
+        <div className="dashboard-right">
+          <Profile />
+        </div>
+      </div>
     </DashboardLayout>
   );
 };
