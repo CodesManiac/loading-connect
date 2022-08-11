cdd104efda67e763df1f63b85bb6b52e6148a574
MICHAEL NWACHUKWU <NUMICHAEL.DEV@GMAIL.COM>
2022-08-12 00:30:39 +0100

    added changes

---
+++ src/pages/app/Messages/DeclineMessage/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import About from '../about/About'
+import DeclineCard from './LogoutCard'
+import './style.scss'
+
+const Decline = () => {
+  return (
+    <div>
+        <About />
+        <DeclineCard />
+    </div>
+  )
+}
+
+export default Decline
